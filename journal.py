0161f1cf1ea0434720825f800d38647250ae1eaa
Harry Stern <hcs@synonymbun.com>
2013-01-14 17:02:24 -0500

    got picky with some superfluous comments and minor pep8 stuff

--- journal.py
+++ journal.py
@@ -14,16 +14,16 @@ try: input = raw_input;  # 2.x and 3.x compatibility hack
 except: pass;
 
 HELPTEXT = """
-journal.py attemps to be a simple-as-possible-but-not-simpler personal diary program using gpg for encryption. To that end, the entire program is contained in a single file, and the only dependencies are this file, python 2.7 or greater, and gpg. An effort has been made to allow almost any text editing program to be used in conjunction with journal.py.
+journal.py attemps to be a simple-as-possible-but-not-simpler personal diary program using gpg for encryption. \
+To that end, the entire program is contained in a single file, and the only dependencies are this file, python 2.7 or greater, and gpg.\
+An effort has been made to allow almost any text editing program to be used in conjunction with journal.py.
 """
 
 EPILOG = """
 (c) 2012 Harry Stern and released under the MIT License <http://opensource.org/licenses/MIT>
 """
 
-####--------####
 #    config    #
-####--------####
 JOURNALDIR = "."
 TEMPDIR = None  # uses tempfile.mkstemp()'s default directory when None
 
@@ -32,9 +32,8 @@ JOURNALEXT = ".jrn"
 
 EDITORCMD = "nano %(file)s"
 
-####--------####
-#    journal   #
-####--------####
+
+#    journal    #
 def create_tempfile():
     #create temp file, but we're just passing the file name to another program
     #mostly it's for the os-independent file creation
@@ -44,19 +43,18 @@ def create_tempfile():
 
     return tempf
 
-def del_tempfile(temp_file):
 
-    #first, try using shred
+def del_tempfile(temp_file):
     #shred is not a panacea; in particular, on some journaled filesystems it is kind of useless
     #unless you can guarantee the fs journal is also overwritten
     try:
         subprocess.call(['shred', temp_file])  # i am probably missing some important option or something here
     except OSError as err:
-        if err.errno == 2:
-            pass
+        if type(err) == FileNotFoundError:
+            pass # XXX maybe we should warn about not having shred
         else:
             raise err
-    #XXX find out what to do on windows
+    #XXX find out shred's equivalent on windows
 
     try:
         os.unlink(temp_file)
@@ -65,23 +63,25 @@ def del_tempfile(temp_file):
 
 #---
 
+
 def encrypt(temp_file, entry_file):
-    #--no-use-agent is because gpg complains when gpg-agent isn't running
-    #--no-mdc-warning is so that gpg doesn't complain about MDC integrity protection (which is irrelevant)
+    #--no-use-agent is because we don't need gpg-agent
+    #--no-mdc-warning is because MDC integrity protection is irrelevant in our case
     #--quiet suppresses messages telling you what algorithm the file is encrypted with
     #XXX should add option for other ciphers, though really you should configure that in .gnupg/gpg.conf
     subprocess.call(["gpg", "--no-use-agent", "--output", entry_file, "--symmetric", temp_file])
 
+
 def decrypt(entry_file, temp_file):
-    #--yes is because we create the tempfile outside of gpg for convenience but then it asks "do you want to overwrite"
-    #the empty file we just created
+    #--yes is because gpg asks "do you want to overwrite"
+    #the empty temp file we just created
     subprocess.call(["gpg", "--no-use-agent", "--no-mdc-warning", "--yes", "--quiet", "--output", temp_file, "--decrypt", entry_file])
 
 #---
 
+
 def spawn_editor(temp_file):
     try:
-        #file name is not always the last argument, shlex.split should be fine even for windows
         command = shlex.split(EDITORCMD % {"file": temp_file})
         subprocess.call(command)
     except OSError as err:
@@ -96,13 +96,13 @@ def spawn_editor(temp_file):
 
 #---
 
+
 def _get_entry_file(journal_dir):
-    #file name is year month day hour minute in such a way that when sorted alphanumerically, oldest comes first
     fname = time.strftime(JOURNALFMT) + JOURNALEXT
     return os.path.join(os.path.abspath(journal_dir), fname)
 
-def new_entry(args):
 
+def new_entry(args):
     temp_file = create_tempfile()
 
     #check options, probably could do this elsewhere after restructuring
@@ -122,12 +122,13 @@ def new_entry(args):
 
 #---
 
+
 def _get_latest_entry(journal_dir):
     #get all files in journal directory whose extension is the right one
     files = (f for f in os.listdir(journal_dir) if os.path.splitext(f)[1] == JOURNALEXT)
 
     #return a sorted list of the last modified times of the files, largest (i.e. most recent) first
-    times = sorted(((f, os.path.getmtime(f)) for f in files), key=lambda f: f[1], reverse=True)
+    times = sorted(((f, os.path.getmtime(f)) for f in files), key=lambda l: l[1], reverse=True)
 
     if times:
         return times[0][0]
@@ -135,10 +136,10 @@ def _get_latest_entry(journal_dir):
         print("No journal entries found. Exiting.")
         sys.exit()
 
+
 def view_entry(args):
     temp_file = create_tempfile()
 
-    #options
     journal_dir = args.dir
     entry_file = args.entry
 
@@ -154,39 +155,45 @@ def view_entry(args):
 
 #---
 
+
 def edit_entry():
     pass
 
 
-####--------####
 #    parser    #
-####--------####
 parser = argparse.ArgumentParser(description=HELPTEXT, epilog=EPILOG)
 subparsers = parser.add_subparsers()
 
-#the problem with subparsers is that they are mandatory and you can't just output help if none are used
+#the problem with subparsers is that you can't just output help if none are used
 #XXX fix somehow? maybe just try/except: pass;
 #also they're kind of convoluted and stupid
 
 ##journal.py add [args]
-add_entry_parser = subparsers.add_parser("add", help="Create a journal entry")
+add_entry_parser = subparsers.add_parser("add", help="Create a journal entry.")
 add_entry_parser.set_defaults(cmd=new_entry)
 
 #options
-add_entry_parser.add_argument("-d", "--dir", default=JOURNALDIR, help="Directory in which the journal files are stored. Default is current directory.")
-add_entry_parser.add_argument("-o", "--output", help="Journal entry file output name. Default is YYYYMMDDHHMM.jrn.")
+add_entry_parser.add_argument("-d", "--dir", default=JOURNALDIR,
+        help="Directory in which the journal files are stored. Default is current directory.")
+
+add_entry_parser.add_argument("-o", "--output",
+        help="Journal entry file output name. Default is YYYYMMDDHHMM.jrn.")
 
 ##journal.py view [args]
-view_entry_parser = subparsers.add_parser("view", help="View a journal entry")
+view_entry_parser = subparsers.add_parser("view", help="View a journal entry.")
 view_entry_parser.set_defaults(cmd=view_entry)
 
 #options
-view_entry_parser.add_argument("-d", "--dir", default=JOURNALDIR, help="Directory in which the journal files are stored. Default is current directory.")
-view_entry_parser.add_argument("entry", help="Journal entry id to view. Default is latest.")
+view_entry_parser.add_argument("-d", "--dir", default=JOURNALDIR,
+        help="Directory in which the journal files are stored. Default is current directory.")
+
+view_entry_parser.add_argument("entry",
+        help="Journal entry id to view. Default is latest.")
 
 ##journal.py edit [args]
-edit_entry_parser = subparsers.add_parser("edit", help="Edit a journal entry")
+edit_entry_parser = subparsers.add_parser("edit", help="Edit a journal entry".)
 edit_entry_parser.set_defaults(cmd=edit_entry)
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
